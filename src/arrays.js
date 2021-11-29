866870b7c5eb73f2800de899c3731e0e51c0d73f
Shaun Emery <shaunsheep18@gmail.com>
2021-11-29 10:08:18 +0000

    revising exercises after solutions

--- src/arrays.js
+++ src/arrays.js
@@ -3,23 +3,24 @@ const getNthElement = (index, array) => {
   return array[index%array.length];
 };
 
-const arrayToCSVString = array => {
-  let newArray = '';
-  for (let i = 0; i<array.length; i++){
-    newArray += array[i];
-    if (i<(array.length -1)){
-      newArray += ",";
-    }
-  }
-  return newArray;
-};
+const arrayToCSVString = array => array.join();
+  //let newArray = '';
+  //for (let i = 0; i<array.length; i++){
+   // newArray += array[i];
+   // if (i<(array.length -1)){
+  //    newArray += ",";
+   // }
+//}
+  //return newArray;
+  
+;
 
-const csvStringToArray = string => {
-  let array = string.split('');
-  const newArray = array.filter(element => element !== ",");
-  console.log(newArray);
-  return newArray;
-};
+const csvStringToArray = string => string.split(',');
+ // let array = string.split('');
+ // const newArray = array.filter(element => element !== ",");
+ // console.log(newArray);
+ // return newArray;
+
 
 const addToArray = (element, array) => {
   array.push(element);
@@ -43,90 +44,101 @@ const numbersToStrings = numbers => {
 };
 
 const uppercaseWordsInArray = strings => {
-  for (let i = 0; i <strings.length; i++) {
-    strings[i] = strings[i].toUpperCase();
-  }
-  return strings;
+  //for (let i = 0; i <strings.length; i++) {
+   // strings[i] = strings[i].toUpperCase();
+  //}
+  //return strings;
+  return strings.map(string => string.toUpperCase());
 };
 
 const reverseWordsInArray = strings => {
-  let reverseArray =[];
-  for (i = 0; i < strings.length; i++) {
-    let reverseString = '';
-    let word = strings[i];
-    for (j = word.length - 1; j >= 0; j--) {
-       reverseString += word[j];
-    }
-    reverseArray.push(reverseString);
-  }
-  return reverseArray;
+//  let reverseArray =[];
+//  for (i = 0; i < strings.length; i++) {
+//    let reverseString = '';
+//    let word = strings[i];
+//    for (j = word.length - 1; j >= 0; j--) {
+//       reverseString += word[j];
+ //   }
+//    reverseArray.push(reverseString);
+//  }
+//  return reverseArray;
+  return strings.map(string => string.split('').reverse().join(''))
 };
 
-const onlyEven = numbers => {
-  console.log(numbers);
-  let newArray=[];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i]%2 === 0) {
-      newArray.push(numbers[i]);
-    };
-  }
-  console.log(newArray);
-  return newArray;
-};
+const onlyEven = numbers => numbers.filter(number => number % 2 === 0)
+  //console.log(numbers);
+  //let newArray=[];
+  //for (let i = 0; i < numbers.length; i++) {
+   // if (numbers[i]%2 === 0) {
+   //   newArray.push(numbers[i]);
+   // };
+  //}
+  //console.log(newArray);
+  //return newArray;
+;
 
 const removeNthElement2 = (index, array) => {
-  let newArray = [];
-  for (let i =0; i<array.length; i++) {
-    if(i !== index) {
-      newArray.push(array[i]);
-    }
-  } 
-  return newArray;
+  //let newArray = [];
+  //for (let i =0; i<array.length; i++) {
+   // if(i !== index) {
+   //   newArray.push(array[i]);
+  //  }
+ // } 
+  //return newArray;
 };
 
 const elementsStartingWithAVowel = strings => {
-  let newArray = [];
-  for (let i = 0; i< strings.length; i++) {
-    let x = strings[i].toLowerCase();
-    if (x.startsWith('a') || (x.startsWith('e')) || (x.startsWith('i')) ||
-     (x.startsWith('o')) || (x.startsWith('u'))) {
-       newArray.push(strings[i]);
-     }
+  //let newArray = [];
+  //for (let i = 0; i< strings.length; i++) {
+   // let x = strings[i].toLowerCase();
+    //if (x.startsWith('a') || (x.startsWith('e')) || (x.startsWith('i')) ||
+    // (x.startsWith('o')) || (x.startsWith('u'))) {
+    //   newArray.push(strings[i]);
+   //  }
      
-  };
-  return newArray;
+ //};
+  //return newArray;
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  return strings.filter(word => {
+    const firstLetter = word[0].toLowerCase();
+    return vowels.includes(firstLetter);
+  });
 };
 
 
-const removeSpaces = string => {
-  let newString = '';
-  for (let i = 0; i <string.length; i++){
-    if (string[i] !== ' '){
-      newString += (string[i]);
-    };
-  }
-  return newString;
-};
+const removeSpaces = string => string.split(' ').join('');
+  //let newString = '';
+  //for (let i = 0; i <string.length; i++){
+   // if (string[i] !== ' '){
+   //   newString += (string[i]);
+   // };
+  //}
+  //return newString;
+;
 
-const sumNumbers = numbers => {
-  let total = 0;
-  for (i=0; i< numbers.length; i++) {
-    total += numbers[i];
-  }
-  return total;
-};
+const sumNumbers = numbers => numbers.reduce((total, number) => {
+  return total + number
+}, 0)
+  //let total = 0;
+  //for (i=0; i< numbers.length; i++) {
+  //  total += numbers[i];
+  //}
+  //return total;
+;
 
 const sortByLastLetter = strings => {
-    strings.sort (function (a, b) {
-      if (a[a.length -1] < b[b.length -1]){
-      return -1;
-      }
-      if (a[a.length -1] > b[b.length -1]){
-      return 1;
-      }
-    });
-    console.log(strings);
-    return strings;
+    //strings.sort (function (a, b) {
+    //  if (a[a.length -1] < b[b.length -1]){
+     // return -1;
+     // }
+     // if (a[a.length -1] > b[b.length -1]){
+     // return 1;
+     // }
+    //});
+    //console.log(strings);
+    //return strings;
+    const reverseString = (item) => item.split('').reverse().join('');
+    return strings.map(reverseString).sort().map(reverseString);
 };
 
 module.exports = {
--- src/objects.js
+++ src/objects.js
@@ -22,37 +22,41 @@ const isOver65 = person => {
   return person.age>65;
 };
 
-const getAges = people => {
-  let newArray=[];
-  for (i=0; i<people.length; i++) {
-  newArray.push(people[i].age)}
-  return newArray;
-};
+const getAges = people => people.map(person => person.age)
+  // let newArray=[]; //
+  // for (i=0; i<people.length; i++) {
+  //newArray.push(people[i].age)}
+  //return newArray; 
 
-const findByName = (name, people) => {
-  for (let i =0; i < people.length; i++) {
-    if(people[i].name === name) {
-      return people[i];
-    }
-  }
-};
+;
 
-const findHondas = cars => {
-  let newArray=[];
-  for (let i = 0; i < cars.length; i++){
-    if (cars[i].manufacturer === "Honda"){
-      newArray.push(cars[i]);
-    }
-  }
-  return newArray;
-};
+const findByName = (name, people) => people.find(person => person.name === name)
+  //for (let i =0; i < people.length; i++) {
+  //  if(people[i].name === name) {
+  //    return people[i];
+  //  }
+  //}
+;
+
+const findHondas = cars => cars.filter(obj => obj.manufacturer === "Honda")
+  //let newArray=[];
+  //for (let i = 0; i < cars.length; i++){
+   // if (cars[i].manufacturer === "Honda"){
+  //    newArray.push(cars[i]);
+   // }
+  //}
+  //return newArray;
+;
 
 const averageAge = people => {
-  let ageTotal=0;
-  for (let i = 0; i<people.length; i++){
-    ageTotal += people[i].age;
-  }
-  return ageTotal/people.length;
+  //let ageTotal=0;
+  //for (let i = 0; i<people.length; i++){
+  //  ageTotal += people[i].age;
+  //}
+  // return ageTotal/people.length;
+  const totalAge= people.reduce((prevAge, currentAge) => {
+    return prevAge + currentAge.currentAge}, 0);
+    return totalAge/people.length;
 };
 
 const createTalkingPerson = (name, age) => {
